5c9bb1349301eff30ca6b461997dd81f686e9300
Marvin Frachet <marvin.frachet@gmail.com>
2019-07-19 16:27:12 -0400

    works

--- commands/start.js
+++ commands/start.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const { exec } = require("child_process");
 const bs = require("browser-sync").create();
 const logger = require("../utils/logger");
+const debounce = require("debounce");
+
 const dir_of_interest = process.cwd();
 
 const startHttpServer = () => {
@@ -21,25 +23,32 @@ const startHttpServer = () => {
 
 const reloadApp = () => bs.reload("*.*");
 
+const handleWatch = (eventType, fileName) => {
+  if (!fileName.includes(".wasm")) {
+    logger.load(`⏳  Change found on ${fileName}, building the new version...`);
+
+    exec("GOOS=js GOARCH=wasm go build -o main.wasm", err => {
+      if (err) {
+        return logger.error(`❌  ${err.toString()}`);
+      }
+
+      logger.succeed(`✅  Building done for ${fileName} changes`);
+
+      reloadApp();
+    });
+  }
+};
+
+const debouncedWatch = debounce(handleWatch, 300);
+
 const start = () => {
   logger.log(
-    `The current folder is now under watch for modifications. Application started on http://localhost:1234`
+    `😎  The current folder is now under watch for modifications. Application started on http://localhost:1234`
   );
 
   startHttpServer();
 
-  fs.watch(process.cwd(), (eventType, fileName) => {
-    if (!fileName.includes(".wasm")) {
-      logger.load(`Change found on ${fileName}, building the new version...`);
-      exec("GOOS=js GOARCH=wasm go build -o main.wasm", err => {
-        if (err) {
-          return logger.error(err.toString());
-        }
-        logger.validate("Building done");
-        reloadApp();
-      });
-    }
-  });
+  fs.watch(process.cwd(), debouncedWatch);
 };
 
 module.exports = start;

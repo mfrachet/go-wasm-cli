36d1a368ae2f9516f30867d4dee5b3d9df758268
Marvin Frachet <marvin.frachet@gmail.com>
2019-07-19 15:54:56 -0400

    removing watchman

--- commands/start.js
+++ commands/start.js
@@ -1,84 +1,44 @@
-const watchman = require('fb-watchman');
-const { exec } = require('child_process');
-const bs = require('browser-sync').create();
-const logger = require('../utils/logger');
+const fs = require("fs");
+const { exec } = require("child_process");
+const bs = require("browser-sync").create();
+const logger = require("../utils/logger");
 const dir_of_interest = process.cwd();
 
 const startHttpServer = () => {
   bs.init({
     notify: false,
-    logLevel: 'silent',
+    logLevel: "silent",
     server: dir_of_interest,
     callbacks: {
       ready: (err, browsersync) => {
         browsersync.utils.serveStatic.mime.define({
-          'application/wasm': ['wasm']
+          "application/wasm": ["wasm"]
         });
       }
     }
   });
 };
 
-const reloadApp = () => bs.reload('*.*');
+const reloadApp = () => bs.reload("*.*");
 
-const makeSubscription = (client, watch, relative_path) => {
-  const sub = {
-    expression: ['allof', ['match', '*.go']],
-    fields: ['name', 'size', 'exists', 'type']
-  };
+const start = () => {
+  logger.log(
+    `The current folder is now under watch for modifications. Application started on http://localhost:1234`
+  );
 
-  if (relative_path) {
-    sub.relative_root = relative_path;
-  }
+  startHttpServer();
 
-  client.command(['subscribe', watch, 'file-watching', sub], error => {
-    if (error) {
-      logger.error('Failed to subscribe: ', error);
-    }
-  });
-
-  client.on('subscription', resp => {
-    if (resp.subscription === 'file-watching') {
-      process.stdout.clearLine();
-      process.stdout.cursorTo(0);
-      logger.load('Now building...');
-
-      exec('GOOS=js GOARCH=wasm go build -o main.wasm', err => {
-        process.stdout.clearLine();
-        process.stdout.cursorTo(0);
+  fs.watch(process.cwd(), (eventType, fileName) => {
+    if (!fileName.includes(".wasm")) {
+      logger.load(`Change found on ${fileName}, building the new version...`);
+      exec("GOOS=js GOARCH=wasm go build -o main.wasm", err => {
         if (err) {
           return logger.error(err.toString());
         }
-        logger.validate('Building done');
+        logger.validate("Building done");
         reloadApp();
       });
     }
-  });
-};
-
-const start = () => {
-  const client = new watchman.Client();
-
-  client.capabilityCheck({ optional: [], required: ['relative_root'] }, error => {
-    if (error) {
-      logger.error(error);
-      return client.end();
-    }
-
-    client.command(['watch-project', dir_of_interest], (error, resp) => {
-      if (error) {
-        throw new Error(error);
-      }
-
-      if ('warning' in resp) {
-        logger.warn(resp.warning);
-      }
-
-      logger.log(`The current folder ${dir_of_interest} is now under watch for modifications. Application started on http://localhost:1234`);
-
-      startHttpServer();
-      makeSubscription(client, resp.watch, resp.relative_path);
-    });
   });
 };
 
